d00b39e20236b9e89fb6437a6e4eb021724d4e71
GoomezDaniel <65666435+GoomezDaniel@users.noreply.github.com>
2023-03-17 15:38:08 +0100

    Change type of petition data

--- lib/providers/cards_data_provider.dart
+++ lib/providers/cards_data_provider.dart
@@ -1,15 +1,10 @@
-import 'dart:convert';
-import 'dart:math';
-
-import 'package:magic_test/models/magic_data_model.dart';
+import 'package:http/http.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:magic_test/utils/constants.dart';
 
 class MagicProvider {
-  Future<MagicDataModel> loadCardData(int page) async {
-    final response = await http.get(Uri.parse('$httpPettionData$page'));
-
-    return MagicDataModel.fromJson(jsonDecode(response.body));
+  Future<Response> loadCardData(int page) async {
+    return await http.get(Uri.parse('$httpPettionData$page'));
   }
 }
