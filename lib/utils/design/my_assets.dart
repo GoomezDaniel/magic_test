c62878ed2f58831cf1063ae1976cadad4eeee239
GoomezDaniel <65666435+GoomezDaniel@users.noreply.github.com>
2023-03-20 13:28:33 +0100

    Added assets

--- lib/utils/design/my_assets.dart
+++ lib/utils/design/my_assets.dart
@@ -1,1 +1,8 @@
-class MyAssets {}
+/// Assets path
+class MyAssets {
+  /// The logo asset
+  static const logo = 'assets/images/logo.png';
+
+  /// The loading asset
+  static const loading = 'assets/icons/loading.gif';
+}
