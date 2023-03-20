53467eb8cebefc38ae0a0c55a164d793ab681889
GoomezDaniel <65666435+GoomezDaniel@users.noreply.github.com>
2023-03-20 13:31:05 +0100

    Fixed show image

--- lib/screens/detail_page.dart
+++ lib/screens/detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:magic_test/utils/design/my_assets.dart';
 import 'package:magic_test/utils/design/my_styles.dart';
 import 'package:magic_test/widgets/type_and_mana_data.dart';
 import 'package:sizer/sizer.dart';
@@ -15,6 +16,10 @@ class DetailPage extends StatelessWidget {
       builder: (context, state) {
         final card = state.cardSelected!;
         return Scaffold(
+          appBar: AppBar(
+            title: const Text('Magic Detail'),
+            centerTitle: true,
+          ),
           body: SafeArea(
             child: Sizer(
               builder: (context, orientation, deviceType) {
@@ -24,17 +29,22 @@ class DetailPage extends StatelessWidget {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
-                        if (card.imageUrl!.isNotEmpty) ...[
+                        if (card.imageUrl != null) ...[
                           SizedBox(
-                              width: MediaQuery.of(context).size.width / 2.5,
-                              child: Image.network(card.imageUrl!)),
+                            width: MediaQuery.of(context).size.width / 2.5,
+                            child: FadeInImage(
+                              placeholder: const AssetImage(MyAssets.loading),
+                              image: NetworkImage(
+                                card.imageUrl!.replaceAll('http', 'https'),
+                              ),
+                            ),
+                          ),
                         ],
                         SizedBox(
                           width: MediaQuery.of(context).size.width / 2.5,
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              const SizedBox(height: 10),
                               Text(card.name!, style: MyStyles.title),
                               const SizedBox(height: 10),
                               Text(card.text!, style: MyStyles.subtitle),
@@ -53,8 +63,13 @@ class DetailPage extends StatelessWidget {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      if (card.imageUrl!.isNotEmpty) ...[
-                        Image.network(card.imageUrl!),
+                      if (card.imageUrl != null) ...[
+                        FadeInImage(
+                          placeholder: const AssetImage(MyAssets.loading),
+                          image: NetworkImage(
+                            card.imageUrl!.replaceAll('http', 'https'),
+                          ),
+                        ),
                       ],
                       const SizedBox(height: 10),
                       Text(card.name!, style: MyStyles.title),
